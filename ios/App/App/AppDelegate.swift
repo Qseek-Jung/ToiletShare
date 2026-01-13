84b5d56cb4c8eea3fdd7f6b975a2c062bb2bf62a
Qseek77 <qseek77@gmail.com>
2026-01-13 15:30:21 +0900

    Build 41: Fix Kakao login crash - Add manual SDK init in AppDelegate

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Capacitor
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +10,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Initialize Kakao SDK
+        if let key = Bundle.main.infoDictionary?["KAKAO_APP_KEY"] as? String {
+            KakaoSDK.initSDK(appKey: key)
+        }
+        
         return true
     }
 
@@ -36,6 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
+        
+        // Handle Kakao Login Callback
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
